f7d7a05e9558e2d1745d4e11624944e6b1111ac8
ModyQyW <wurui-dev@foxmail.com>
2023-07-26 10:36:36 +0800

    ci: add cicada for a try

---
+++ .cicada/ci.ts
@@ -0,0 +1,34 @@
+import { Job, Pipeline } from 'https://deno.land/x/cicada@v0.1.50/mod.ts';
+
+const lintJob = new Job({
+  name: 'lint',
+  image: 'node:18-alpine',
+  cacheDirectories: ['node_modules'],
+  steps: [
+    { run: 'corepack enable' },
+    { run: 'pnpm install' },
+    { run: 'pnpm run build' },
+    { run: 'pnpm run lint' },
+  ],
+});
+const typecheckJob = new Job({
+  name: 'typecheck',
+  image: 'node:18-alpine',
+  cacheDirectories: ['node_modules'],
+  steps: [{ run: 'corepack enable' }, { run: 'pnpm install' }, { run: 'pnpm run check:types' }],
+});
+
+const testJob = new Job({
+  name: 'test',
+  image: 'node:18-alpine',
+  cacheDirectories: ['node_modules'],
+  steps: [{ run: 'corepack enable' }, { run: 'pnpm install' }, { run: 'pnpm run test' }],
+});
+
+export default new Pipeline([lintJob, typecheckJob, testJob], {
+  name: 'ci',
+  on: {
+    pullRequest: ['main'],
+    push: ['main'],
+  },
+});
