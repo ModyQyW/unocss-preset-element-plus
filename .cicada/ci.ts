e6de1ebeed9e767480d1aa6048c8a6360d9ecd56
ModyQyW <wurui-dev@foxmail.com>
2023-07-26 13:45:32 +0800

    ci: update cicada config

--- .cicada/ci.ts
+++ .cicada/ci.ts
@@ -2,7 +2,7 @@ import { Job, Pipeline } from 'https://deno.land/x/cicada@v0.1.50/mod.ts';
 
 const lintJob = new Job({
   name: 'lint',
-  image: 'node:18-alpine',
+  image: 'node:18',
   cacheDirectories: ['node_modules'],
   steps: [
     { run: 'corepack enable' },
@@ -13,14 +13,14 @@ const lintJob = new Job({
 });
 const typecheckJob = new Job({
   name: 'typecheck',
-  image: 'node:18-alpine',
+  image: 'node:18',
   cacheDirectories: ['node_modules'],
   steps: [{ run: 'corepack enable' }, { run: 'pnpm install' }, { run: 'pnpm run check:types' }],
 });
 
 const testJob = new Job({
   name: 'test',
-  image: 'node:18-alpine',
+  image: 'node:18',
   cacheDirectories: ['node_modules'],
   steps: [{ run: 'corepack enable' }, { run: 'pnpm install' }, { run: 'pnpm run test' }],
 });
