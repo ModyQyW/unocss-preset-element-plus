5e7448a6238a3243d6132945d1b9eacd6de39e0a
ModyQyW <wurui-dev@foxmail.com>
2023-08-18 21:00:49 +0800

    test: update font-size tests

--- tests/font-size.test.ts
+++ tests/font-size.test.ts
@@ -98,4 +98,27 @@ describe('theme', () => {
       .text-xs{font-size:var(--un-preset-el-fontSize-xs);line-height:1;}"
     `);
   });
+
+  test('override some font size', async () => {
+    const uno = createGenerator({
+      presets: [
+        presetUno(),
+        presetElementPlus({ overrideFontSize: ['base', 'xs', 'sm', 'md', 'lg'] }),
+      ],
+    });
+    const targets = ['text-base', 'text-xs', 'text-sm', 'text-md', 'text-lg'];
+    const { css } = await uno.generate(targets.join('\n'));
+    expect(css).toMatchInlineSnapshot(`
+      "/* layer: preflights */
+      *,::before,::after{--un-rotate:0;--un-rotate-x:0;--un-rotate-y:0;--un-rotate-z:0;--un-scale-x:1;--un-scale-y:1;--un-scale-z:1;--un-skew-x:0;--un-skew-y:0;--un-translate-x:0;--un-translate-y:0;--un-translate-z:0;--un-pan-x: ;--un-pan-y: ;--un-pinch-zoom: ;--un-scroll-snap-strictness:proximity;--un-ordinal: ;--un-slashed-zero: ;--un-numeric-figure: ;--un-numeric-spacing: ;--un-numeric-fraction: ;--un-border-spacing-x:0;--un-border-spacing-y:0;--un-ring-offset-shadow:0 0 rgba(0,0,0,0);--un-ring-shadow:0 0 rgba(0,0,0,0);--un-shadow-inset: ;--un-shadow:0 0 rgba(0,0,0,0);--un-ring-inset: ;--un-ring-offset-width:0px;--un-ring-offset-color:#fff;--un-ring-width:0px;--un-ring-color:rgba(147,197,253,0.5);--un-blur: ;--un-brightness: ;--un-contrast: ;--un-drop-shadow: ;--un-grayscale: ;--un-hue-rotate: ;--un-invert: ;--un-saturate: ;--un-sepia: ;--un-backdrop-blur: ;--un-backdrop-brightness: ;--un-backdrop-contrast: ;--un-backdrop-grayscale: ;--un-backdrop-hue-rotate: ;--un-backdrop-invert: ;--un-backdrop-opacity: ;--un-backdrop-saturate: ;--un-backdrop-sepia: ;}::backdrop{--un-rotate:0;--un-rotate-x:0;--un-rotate-y:0;--un-rotate-z:0;--un-scale-x:1;--un-scale-y:1;--un-scale-z:1;--un-skew-x:0;--un-skew-y:0;--un-translate-x:0;--un-translate-y:0;--un-translate-z:0;--un-pan-x: ;--un-pan-y: ;--un-pinch-zoom: ;--un-scroll-snap-strictness:proximity;--un-ordinal: ;--un-slashed-zero: ;--un-numeric-figure: ;--un-numeric-spacing: ;--un-numeric-fraction: ;--un-border-spacing-x:0;--un-border-spacing-y:0;--un-ring-offset-shadow:0 0 rgba(0,0,0,0);--un-ring-shadow:0 0 rgba(0,0,0,0);--un-shadow-inset: ;--un-shadow:0 0 rgba(0,0,0,0);--un-ring-inset: ;--un-ring-offset-width:0px;--un-ring-offset-color:#fff;--un-ring-width:0px;--un-ring-color:rgba(147,197,253,0.5);--un-blur: ;--un-brightness: ;--un-contrast: ;--un-drop-shadow: ;--un-grayscale: ;--un-hue-rotate: ;--un-invert: ;--un-saturate: ;--un-sepia: ;--un-backdrop-blur: ;--un-backdrop-brightness: ;--un-backdrop-contrast: ;--un-backdrop-grayscale: ;--un-backdrop-hue-rotate: ;--un-backdrop-invert: ;--un-backdrop-opacity: ;--un-backdrop-saturate: ;--un-backdrop-sepia: ;}
+      /* layer: theme */
+      .dark,:root{--un-preset-el-fontSize-base:var(--el-font-size-base);--un-preset-el-fontSize-xs:var(--el-font-size-extra-small);--un-preset-el-fontSize-sm:var(--el-font-size-small);--un-preset-el-fontSize-md:var(--el-font-size-medium);--un-preset-el-fontSize-lg:var(--el-font-size-large);}
+      /* layer: default */
+      .text-base{font-size:var(--un-preset-el-fontSize-base);line-height:1;}
+      .text-lg{font-size:var(--un-preset-el-fontSize-lg);line-height:1;}
+      .text-md{font-size:var(--un-preset-el-fontSize-md);line-height:1;}
+      .text-sm{font-size:var(--un-preset-el-fontSize-sm);line-height:1;}
+      .text-xs{font-size:var(--un-preset-el-fontSize-xs);line-height:1;}"
+    `);
+  });
 });
